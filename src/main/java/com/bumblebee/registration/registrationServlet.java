8cb9af1b00c5382141a73ca7c87d199baa1d0926
nimash eshan <124778864+Senanayake-R@users.noreply.github.com>
2023-02-10 02:25:10 +0530

    check servlet

--- src/main/java/com/bumblebee/registration/registrationServlet.java
+++ src/main/java/com/bumblebee/registration/registrationServlet.java
@@ -1,21 +1,24 @@
 package com.bumblebee.registration;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequest; 
 import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class registrationServlet
  */
-@WebServlet("/registrationServlet")
+@WebServlet("/register")
 public class registrationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		PrintWriter out = response.getWriter();
+		out.print("working");
 	}
 
 }
