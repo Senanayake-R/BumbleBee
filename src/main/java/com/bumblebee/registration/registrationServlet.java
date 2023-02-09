324856497d1cd031c60f35efea1d215f62ce9b28
nimash eshan <124778864+Senanayake-R@users.noreply.github.com>
2023-02-10 02:16:58 +0530

    initial registration servlet

---
+++ src/main/java/com/bumblebee/registration/registrationServlet.java
@@ -0,0 +1,21 @@
+package com.bumblebee.registration;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class registrationServlet
+ */
+@WebServlet("/registrationServlet")
+public class registrationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+}
