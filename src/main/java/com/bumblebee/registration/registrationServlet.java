e2bb9758af2704263eeb10232ac9c867c5b9541f
nimash eshan <124778864+Senanayake-R@users.noreply.github.com>
2023-02-10 03:25:29 +0530

    connected with mysql workbench

--- src/main/java/com/bumblebee/registration/registrationServlet.java
+++ src/main/java/com/bumblebee/registration/registrationServlet.java
@@ -3,6 +3,14 @@ package com.bumblebee.registration;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,8 +25,57 @@ public class registrationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-		out.print("working");
+		String uname = request.getParameter("name");
+		String uemail = request.getParameter("email");
+		String upwd = request.getParameter("pass");
+		String umobile = request.getParameter("contact");
+		
+		RequestDispatcher dispatcher = null;
+		
+		Connection con = null;
+		
+//		PrintWriter out = response.getWriter();
+//		out.print(uname);
+//		out.print(uemail);
+//		out.print(upwd);
+//		out.print(umobile);
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3308/BumbleBee","root","1234");
+			PreparedStatement pst = con.prepareStatement("insert into users(userName, userPwd, userEmail, UserMobile) values(?,?,?,?)");
+			pst.setString(1, uname);
+			pst.setString(2, upwd);
+			pst.setString(3, uemail);
+			pst.setString(4, umobile);
+			
+			int rowCount = pst.executeUpdate();
+			
+			dispatcher = request.getRequestDispatcher("registration.jsp");
+			
+			if(rowCount>0) {
+				request.setAttribute("status", "success");
+				
+			}
+			else{
+				request.setAttribute("status", "failed");
+			}
+			
+			dispatcher.forward(request, response);
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}finally{
+			try {
+				if(con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 	}
 
 }
