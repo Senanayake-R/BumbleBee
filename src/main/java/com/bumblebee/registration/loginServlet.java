e750dae929a687143c3427426d5cd7a67b3e6f28
Senanayake-R <124778864+Senanayake-R@users.noreply.github.com>
2023-02-21 18:38:11 +0530

    added session cookies

--- src/main/java/com/bumblebee/registration/loginServlet.java
+++ src/main/java/com/bumblebee/registration/loginServlet.java
@@ -6,10 +6,12 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.UUID;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,42 +22,49 @@ import javax.servlet.http.HttpSession;
  */
 @WebServlet("/login")
 public class loginServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    private static final long serialVersionUID = 1L;
 
-		String uemail = request.getParameter("username");
-		String upwd = request.getParameter("password");
-		
-		HttpSession session = request.getSession();
-		
-		Connection con = null;
-		
-		RequestDispatcher dispatcher = null;
-		
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3308/BumbleBee?useSSL=false","root","1234");
-			PreparedStatement pst = con.prepareStatement("select * from users where userEmail= ? and userPwd= ?");;
-			pst.setString(1, uemail);
-			pst.setString(2, upwd);
-			
-			ResultSet rs = pst.executeQuery();
-			
-			if(rs.next()) {
-				session.setAttribute("name",rs.getString("username"));
-				dispatcher = request.getRequestDispatcher("index.jsp");
-			}else {
-				request.setAttribute("status", "failed");
-				dispatcher = request.getRequestDispatcher("login.jsp");
-			}
-			
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String uemail = request.getParameter("username");
+        String upwd = request.getParameter("password");
 
-			dispatcher.forward(request, response);
-			
-		}
-		catch (Exception e){
-			e.printStackTrace();
-		}
-	}
+        HttpSession session = request.getSession();
+
+        Connection con = null;
+
+        RequestDispatcher dispatcher = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3308/BumbleBee?useSSL=false","root","1234");
+            PreparedStatement pst = con.prepareStatement("select * from users where userEmail= ? and userPwd= ?");;
+            pst.setString(1, uemail);
+            pst.setString(2, upwd);
+
+            ResultSet rs = pst.executeQuery();
+
+            if(rs.next()) {
+                // Generate a session token
+                String token = UUID.randomUUID().toString();
+
+                // Store the session token in the user's session
+                session.setAttribute("token", token);
+                session.setAttribute("name",rs.getString("username"));
+
+                // Set the session token as a cookie
+                Cookie cookie = new Cookie("token", token);
+                response.addCookie(cookie);
+
+                dispatcher = request.getRequestDispatcher("index.jsp");
+            } else {
+                request.setAttribute("status", "failed");
+                dispatcher = request.getRequestDispatcher("login.jsp");
+            }
+
+            dispatcher.forward(request, response);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
